c9bb212ecd22725a43368f12b634eac655f4aaba
郑杰 <411426414@qq.com>
2021-12-03 02:33:42 +0800

    添加登录组件并修改路由文件

---
+++ Graduation/src/views/Login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    1234567
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+    }
+  },
+  created () {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+.container{}
+</style>
